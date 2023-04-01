25a979d3b74dc9408b55763550c12b010938fb44
gladyas <taltindasforum@gmail.com>
2023-04-01 04:03:43 +0300

    Fourth commit

--- src/test/java/com/translantik/pages/BasePage.java
+++ src/test/java/com/translantik/pages/BasePage.java
@@ -4,5 +4,6 @@ public class BasePage {
     public static void main(String[] args) {
         System.out.println("Third changes");
         System.out.println("fourth change");
+        System.out.println("Fifth");
     }
 }
