5687f81315cd4d50d129cacffe59d341013dd0c8
verdadislitas <sdislitas@gmail.com>
2023-04-01 06:14:09 +0300

    verda trhird commit

--- src/test/java/com/translantik/step_definitions/Glad.java
+++ src/test/java/com/translantik/step_definitions/Glad.java
@@ -3,5 +3,6 @@ package com.translantik.step_definitions;
 public class Glad {
     public static void main(String[] args) {
         System.out.println("glad");
+        System.out.println("commend");
     }
 }
