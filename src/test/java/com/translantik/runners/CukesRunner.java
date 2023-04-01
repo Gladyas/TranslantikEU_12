f5e30e144b09d75dc2f24860ad404b25959f80e0
gladyas <taltindasforum@gmail.com>
2023-04-01 07:21:49 +0300

    CukesRunner

--- src/test/java/com/translantik/runners/CukesRunner.java
+++ src/test/java/com/translantik/runners/CukesRunner.java
@@ -1,7 +1,23 @@
 package com.translantik.runners;
+import io.cucumber.junit.*;
+import org.junit.runner.*;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                //"pretty",
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+        },
+        features = "src/test/resources/features",
+        glue = "com/translantik/step_definitions",
+        dryRun = false,
+        tags = "",
+        publish = false
+
+)
 
 public class CukesRunner {
-    public static void main(String[] args) {
 
-    }
 }
