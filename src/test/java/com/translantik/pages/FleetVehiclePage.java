cc9a197f9dada20561e505ef30514f62202c6da2
Reyhan <113282262+ReyhanTanisik@users.noreply.github.com>
2023-04-03 15:23:45 +0300

    I created my background , added web elements on fleet vehicle page

--- src/test/java/com/translantik/pages/FleetVehiclePage.java
+++ src/test/java/com/translantik/pages/FleetVehiclePage.java
@@ -1,6 +1,32 @@
 package com.translantik.pages;
 
-public class FleetVehiclePage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class FleetVehiclePage extends BasePage {
+
+    @FindBy(name = "_username")
+    public static WebElement inputUsername;
+
+    @FindBy(name = "_password")
+    public static WebElement inputPassword;
+
+    @FindBy(id = "_submit")
+    public static WebElement loginButton;
+
+    public static void loginWithCredentials(String username, String password){
+        inputUsername.sendKeys(username);
+        inputPassword.sendKeys(password);
+        loginButton.click();
+    }
+
+    @FindBy(xpath="//*[@id='main-menu']/ul/li[1]/a/span")
+    public static WebElement fleet;
+
+    @FindBy(xpath="//span[.='Vehicles']")
+    public static WebElement fleetVehicle;
+
+
 
 
 
